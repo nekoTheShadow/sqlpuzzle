808998064bb1adb35821b5223d16cbbc902fe8f7
hajime nakamura <h.nakamura0903@gmail.com>
2021-05-04 20:43:03 +0900

    パズル29 最頻値を求める

---
+++ 29.sql
@@ -0,0 +1,22 @@
+DROP TABLE IF EXISTS Payroll;
+CREATE TABLE Payroll
+(check_nbr INTEGER NOT NULL PRIMARY KEY,
+ check_amt DECIMAL(8,2) NOT NULL);
+
+INSERT INTO Payroll VALUES(1, 100);
+INSERT INTO Payroll VALUES(2, 100);
+INSERT INTO Payroll VALUES(3, 200);
+INSERT INTO Payroll VALUES(4, 150);
+INSERT INTO Payroll VALUES(5, 300);
+INSERT INTO Payroll VALUES(6, 150);
+INSERT INTO Payroll VALUES(7, 300);
+INSERT INTO Payroll VALUES(8, 300);
+INSERT INTO Payroll VALUES(9, 100);
+
+SELECT check_amt, check_cnt
+FROM (
+  SELECT check_amt, COUNT(*) check_cnt, RANK() OVER (ORDER BY COUNT(*) DESC) rnk
+  FROM Payroll
+  GROUP BY check_amt
+) t
+WHERE rnk = 1;
